a99c9a4a3d7c6d718d0d230a9163470623b20801
tybrianna <tybriannahall@gmail.com>
2026-03-19 22:59:53 -0400

    created a way to enter first and last name and to make sure the first letter of each would be capital

---
+++ index.js
@@ -0,0 +1,26 @@
+let firstName = document.getElementById("firstName");
+let lastName = document.getElementById("lastName");
+let submitButton = document.getElementById("submitButton");
+
+function capitalizeFirstLetter(firstName, lastName) {
+  if (firstName.length === 0 || lastName.length === 0) {
+    return '';
+  }
+
+  const capFirst = firstName.charAt(0).toUpperCase() + firstName.slice(1);
+  const capLast = lastName.charAt(0).toUpperCase() + lastName.slice(1);
+
+  return capFirst + " " + capLast;
+}
+
+submitButton.addEventListener("click", function () {
+  const fullName = capitalizeFirstLetter(firstName.value.trim(), lastName.value.trim());
+
+    if (fullName === "") {
+    alert("Please enter your full name.");
+    return;
+  }
+
+  alert(`Welcome ${fullName}`);
+
+});
