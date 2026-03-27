5d642bf1ecc99bfe4b917cc6998d7443099277da
tybrianna <tybriannahall@gmail.com>
2026-03-27 12:07:57 -0400

    added functions and steps to part 2 and three

--- index.js
+++ index.js
@@ -27,10 +27,66 @@ submitButton.addEventListener("click", function () {
 });
 
 // Task Two
-function calculateTotalCost(Price, quantity, taxRate)
+const priceInput = document.getElementById("price");
+const quantityInput = document.getElementById("quantity");
+const taxRateInput = document.getElementById("taxRate");
 
+const calculateBtn = document.getElementById("calculateBtn");
+const answer = document.getElementById("answer");
+
+function calculateTotalCost(price, quantity, taxRate) {
+
+  if (isNaN(price) || isNaN(quantity) || isNaN(taxRate)) {
+    return "Invalid input.";
+  }
+
+  let totalCost = (price * quantity) * (1 + taxRate);
+
+  return totalCost;
+}
+
+calculateBtn.addEventListener("click", function () {
+  
+  let price = Number(priceInput.value);
+  let quantity = Number(quantityInput.value);
+  let taxRate = Number(taxRateInput.value);
+
+  let total = calculateTotalCost(price, quantity, taxRate);
+
+  answer.textContent = total;
+});
 
 // Task Three
+const ageInput = document.getElementById("age");
+const employedInput = document.getElementById("isEmployed");
 
+const checkBtn = document.getElementById("checkBtn");
+const result = document.getElementById("result");
+
+function checkEligibility(age, isEmployed) {
+
+  if (age > 18 && isEmployed === true) {
+    return "Eligible for the program.";
+  }
+
+  else if (age > 18 && isEmployed === false) {
+    return "Conditionally eligible for the program.";
+  }
+
+  else {
+    return "Not eligible for the program.";
+  }
+}
+
+checkBtn.addEventListener("click", function () {
+
+  let age = Number(ageInput.value);
+
+  let isEmployed = employedInput.value.toLowerCase() === "true";
+
+  let message = checkEligibility(age, isEmployed);
+
+  result.textContent = message;
+});
 
 // Task Four
